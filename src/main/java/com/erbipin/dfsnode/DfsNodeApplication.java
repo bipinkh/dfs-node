518deb7c2e6f6aca3e8a7ba30b5ac8bbb0eec34e
bipinkh <bipinkh91@gmail.com>
2018-07-26 19:34:23 +0545

    project initiated

---
+++ src/main/java/com/erbipin/dfsnode/DfsNodeApplication.java
@@ -0,0 +1,12 @@
+package com.erbipin.dfsnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfsNodeApplication.class, args);
+	}
+}
